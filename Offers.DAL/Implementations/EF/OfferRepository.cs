f8969ae25d9bc9cc6727a46e2a814c018905f0e4
artem <artm.isWorking@gmail.com>
2023-02-14 13:34:43 +0300

    create OfferRepository

---
+++ Offers.DAL/Implementations/EF/OfferRepository.cs
@@ -0,0 +1,12 @@
+﻿using Offers.DAL.DataBases;
+using Offers.DAL.Entities;
+
+namespace Offers.DAL.Implementations.EF
+{
+    public class OfferRepository : Repository<Offer>
+    {
+        public OfferRepository(OffersEF offersEF) : base(offersEF)
+        {
+        }
+    }
+}
