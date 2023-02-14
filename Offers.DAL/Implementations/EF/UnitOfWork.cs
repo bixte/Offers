a748d51734844681d583607d11299d8413d3e9b0
artem <artm.isWorking@gmail.com>
2023-02-14 14:06:51 +0300

    create UnitOfWork for Entity framework

---
+++ Offers.DAL/Implementations/EF/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Offers.DAL.DataBases;
+using Offers.DAL.Entities;
+using Offers.DAL.Intefaces.IUnitOfWorks;
+using Offers.DAL.Intefaces.Repositories;
+
+namespace Offers.DAL.Implementations.EF
+{
+    public class UnitOfWorkAsync : IUnitOfWorkAsync
+    {
+        private readonly OffersEF offersEF;
+        public UnitOfWorkAsync(OffersEF offersEF)
+        {
+            this.offersEF = offersEF;
+        }
+        private OfferRepository? offerRepository;
+
+        public IRepositoryAsync<Offer> OfferRepository { 
+            get => offerRepository ??= new OfferRepository(offersEF); 
+        }
+
+        public async Task SaveAsync()
+        {
+           await offersEF.SaveChangesAsync();
+        }
+    }
+}
