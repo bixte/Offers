152fba86459caa0432e1613ec8386e1507eb2cf3
artem <artm.isWorking@gmail.com>
2023-02-14 12:29:09 +0300

    create database context to Entity framework

---
+++ Offers.DAL/DataBases/OffersEF.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Offers.DAL.Entities;
+
+namespace Offers.DAL.DataBases
+{
+    public class OffersEF : DbContext
+    {
+        public OffersEF(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+       public DbSet<Offer> Offers { get; set; }
+    }
+}
