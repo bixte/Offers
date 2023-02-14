fe5547a8ca7516f9c566576afa709aeaa8d7ea90
artem <artm.isWorking@gmail.com>
2023-02-14 13:25:26 +0300

    create interfaces for Repositories

---
+++ Offers.DAL/Intefaces/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Offers.DAL.Intefaces.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        public void Add(T entity);
+        public T? Find(int id);
+        public IEnumerable<T> GetAll();
+        public IEnumerable<T> GetAll(Func<T,bool> predicate);
+        public void Update(T entity);
+        public void Delete(T entity);
+        public void DeleteById(int id);
+    }
+}
---
+++ Offers.DAL/Intefaces/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,8 @@
+﻿namespace Offers.DAL.Intefaces.Repositories
+{
+    public interface IRepositoryAsync<T> : IRepository<T> where T : class
+    {
+        public Task AddAsync(T entity);
+        public Task<T?> FindAsync(int id);
+    }
+}
