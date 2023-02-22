b6f2f6f7ad517a790d1e2c54f490fb1e518a05a0
artem <artm.isWorking@gmail.com>
2023-02-22 16:24:44 +0300

    Add nessary DI  for work

---
+++ Offers.BLL/Infrastructure/AppServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Offers.BLL.Interfaces;
+using Offers.BLL.Services;
+using Offers.DAL.DataBases;
+using Offers.DAL.Implementations.EF;
+using Offers.DAL.Intefaces.IUnitOfWorks;
+
+namespace Offers.BLL.Infrastructure
+{
+    public static class AppServices
+    {
+        public static void AddAppServices(this IServiceCollection services, string? connectionString)
+        {
+            services.AddDbContext<OffersEF>(option => option.UseSqlServer(connectionString));
+            services.AddScoped<IUnitOfWorkAsync, UnitOfWorkAsync>();
+            services.AddScoped<IOfferServiceAsync,OfferService>();
+        }
+    }
+}
