15cdd285127fb67f2c0bf9b2861e6aaf140d3229
artem <artm.isWorking@gmail.com>
2023-02-22 16:18:01 +0300

    Add DTO models

---
+++ Offers.BLL/DTO/OfferAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Offers.BLL.DTO
+{
+    public class OfferAttributeDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Value { get; set; } = null!;
+    }
+}
---
+++ Offers.BLL/DTO/OfferDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Offers.BLL.DTO
+{
+    public class OfferDTO
+    {
+        public int BId { get; set; }
+        public IEnumerable<OfferAttributeDTO> OfferAttribute { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public bool IsAvailable { get; set; }
+        public string Url { get; set; } = null!;
+        public double Price { get; set; }
+        public string Currency { get; set; } = null!;
+        public string? Picture { get; set; }
+        public string Category { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
