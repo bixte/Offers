cb24cdfd40938c97d4f6d339ec55ae1b8c2ee5df
artem <artm.isWorking@gmail.com>
2023-02-22 16:22:25 +0300

    Add interface offer service and implementation.

---
+++ Offers.BLL/Interfaces/IOfferService.cs
@@ -0,0 +1,11 @@
+﻿using Offers.BLL.DTO;
+
+namespace Offers.BLL.Interfaces
+{
+    public interface IOfferServiceAsync
+    {
+        public Task SaveFromOriginAsync(string originURL, string id);
+        public Task<OfferDTO> GetOfferAsync(int id);
+        public Task AddOfferAsync(OfferDTO offerDTO);
+    }
+}
---
+++ Offers.BLL/Services/OfferService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Offers.BLL.DTO;
+using Offers.BLL.Interfaces;
+using Offers.BLL.Utility.Binders;
+using Offers.BLL.Utility.OfferParser;
+using Offers.BLL.Utility.Readers;
+using Offers.DAL.Entities;
+using Offers.DAL.Intefaces.IUnitOfWorks;
+
+namespace Offers.BLL.Services
+{
+    public class OfferService : IOfferServiceAsync
+    {
+        private readonly IUnitOfWorkAsync unitOfWork;
+
+        public OfferService(IUnitOfWorkAsync unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task SaveFromOriginAsync(string originURL, string id)
+        {
+            var reader = new XMLOfferReader(originURL);
+            var xOffer = reader.Read(id);
+            var offerParser = new XMLOfferParser(new OFferBinder());
+            var offer = offerParser.Parse(xOffer);
+            await unitOfWork.OfferRepository.AddAsync(offer);
+            await unitOfWork.SaveAsync();
+        }
+        public async Task<OfferDTO> GetOfferAsync(int id)
+        {
+            var offer = await unitOfWork.OfferRepository.FindAsync(id);
+            if (offer == null)
+                throw new Exception("not found");
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Offer, OfferDTO>()
+                        .ForMember(o => o.Category, option => option.MapFrom(o => o.Category.Name))
+                        .ForMember(o => o.Currency, option => option.MapFrom(o => o.Currency.Id));
+                cfg.CreateMap<OfferAttribute, OfferAttributeDTO>();
+
+            });
+            var offerDTO = config.CreateMapper().Map<Offer, OfferDTO>(offer);
+            return offerDTO;
+        }
+
+        public async Task AddOfferAsync(OfferDTO offerDTO)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<OfferDTO, Offer>());
+            var offer = new Mapper(config).Map<OfferDTO, Offer>(offerDTO);
+            await unitOfWork.OfferRepository.AddAsync(offer);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
